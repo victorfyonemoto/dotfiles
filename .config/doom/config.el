3fc238f6d05ddcbf698ed668201541d2baa15a83
Victor Ferreira Yonemoto <victor.softwaredesign@gmail.com>
2025-03-14 15:41:20 -0300

    Update Doom Emacs packages and config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -76,3 +76,9 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(use-package! org-auto-tangle
+  :defer t
+  :hook (org-mode . org-auto-tangle)
+  :config
+  (setq org-auto-tangle-default t))
